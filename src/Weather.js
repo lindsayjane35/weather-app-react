51c8e345d47f02ca94683e22a0c82c4f0b92b406
Lindsay <lindsayjane35@gmail.com>
2024-01-30 20:38:57 -0800

    created WeatherInfo component

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
-import FormattedDate from "./FormattedDate.js";
 import "./Weather.css";
 
 export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
-  const [weatherData, setWeatherData] = useState({});
 
-  function handleReponse(response) {
+  function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
       date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
       city: response.data.name,
     });
   }
@@ -25,63 +26,42 @@ export default function Weather(props) {
     search();
   }
 
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   function search() {
     const apiKey = "58a6775f97527351bf6c6966e209be39";
     let units = "imperial";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleReponse);
-  }
 
-  function updateCity(event) {
-    setCity(event.target.value);
+    axios.get(apiUrl).then(handleResponse);
   }
-  let form = (
-    <form className="search-form" onSubmit={handleSubmit}>
-      <input
-        className="search-field"
-        type="search"
-        placeholder="Enter a city.."
-        autoFocus="on"
-        onChange={updateCity}
-      />
-      <button className="search-button" type="Submit">
-        Search
-      </button>
-    </form>
-  );
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <h2>{city}</h2>
-        {form}
-
-        <div className="row">
-          <div className="col-6">
-            <img src={weatherData.icon} alt={weatherData.description} />
-
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>
-            <span className="unit">°F | C</span>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="search-field"
+                autoFocus="on"
+                onChange={updateCity}
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="search-button" />
+            </div>
           </div>
-
-          <div className="col-6">
-            <ul>
-              <li className="FormattedDate">
-                <FormattedDate date={weatherData.date} />
-              </li>
-              <li>Temperature: {Math.round(weatherData.temperature)}°F</li>
-              <li>Description: {weatherData.description}</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind}mph</li>
-            </ul>
-          </div>
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
     search();
-    return "loading...";
+    return "Loading...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,31 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <img src="" alt="weather icon" />
+
+            <div>{Math.round(props.data.temperature)}°F</div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
